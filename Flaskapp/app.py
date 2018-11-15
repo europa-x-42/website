6835f5cd111446a9b4253f704a7fafff7da7bbcd
Adrian Sanjo <jesuisanonime@gmail.com>
2018-11-15 09:05:12 +0100

    Added a few placeholder templates

--- Flaskapp/app.py
+++ Flaskapp/app.py
@@ -8,6 +8,33 @@ app=Flask(__name__)
 conn=sqlite3.connect(":memory:") #TODO change to actual file
 db=conn.cursor()
 
+username=None
+admin=False
+
+@app.route("/",methods=["GET"])
+def indexPage():
+    return render_template("index.html",username=username,admin=admin)
+
+@app.route("/catalog/",methods=["GET"])
+def catalogPage():
+    return render_template("catalog.html",username=username,admin=admin)
+
+@app.route("/copyright/",methods=["GET"])
+def copyrightPage():
+    return render_template("copyright.html",username=username,admin=admin)
+
+@app.route("/onlineshop/",methods=["GET"])
+def onlineShopPage():
+    return render_template("onlineshop.html",username=username,admin=admin)
+
+@app.route("/signin/",methods=["GET"])
+def signInPage():
+    return render_template("signin.html",username=username,admin=admin)
+
+@app.route("/signup/",methods=["GET"])
+def signUpPage():
+    return render_template("signup.html",username=username,admin=admin)
+
 if __name__=="__main__":
     try:
         app.debug = True
