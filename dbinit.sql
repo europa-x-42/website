b772b3cd8234483cb4e7dacf1f836b553909844e
Adrian Sanjo <jesuisanonime@gmail.com>
2018-11-17 10:43:36 +0100

    Added database initialisation script

---
+++ dbinit.sql
@@ -0,0 +1,2 @@
+CREATE TABLE Users (username TEXT,email TEXT,password TEXT,isAdmin BOOLEAN);
+INSERT INTO Users (username,email,password,isAdmin) VALUES ('admin','europax@yopmail.com','92b5fe2e600aa6a33b0cd0deeb07e1b34bcf9c997e9a2443769ffa8e1fb878e2',False);
