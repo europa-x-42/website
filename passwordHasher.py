806183d11298822dd7db35299b90d16708e54cd0
Adrian Sanjo <jesuisanonime@gmail.com>
2018-12-05 12:13:33 +0100

    passwordHasher.py refactored

--- passwordHasher.py
+++ passwordHasher.py
@@ -2,10 +2,15 @@
 
 import hashlib,binascii,getpass
 
-username=input("Username: ")
-password=getpass.getpass("Password: ")
-salt="qd>BqùoJDBNùLJQDNBùeqnvgùqV%QBDv%JQLNBqBVmkqjdbvkljQD,N"+username+"AOZDBObÙOUojbnegozgboQegbIgojHqouehNObNiuJNgouinboBOQBGpioBGouNbO<NojbnoujbGUOjivbOULNV"
-salt=hashlib.sha256(bytes(salt,"UTF-8")).hexdigest()
-password=hashlib.pbkdf2_hmac('sha256',bytes(password,"UTF-8"),bytes(salt,"UTF-8"),100000) #type:ignore
-password=str(binascii.hexlify(password),"UTF-8") #type:ignore
-print(password)
+
+def hashPassword(username,password):
+    salt="qd>BqùoJDBNùLJQDNBùeqnvgùqV%QBDv%JQLNBqBVmkqjdbvkljQD,N"+username+"AOZDBObÙOUojbnegozgboQegbIgojHqouehNObNiuJNgouinboBOQBGpioBGouNbO<NojbnoujbGUOjivbOULNV"
+    salt=hashlib.sha256(bytes(salt,"UTF-8")).hexdigest()
+    password=hashlib.pbkdf2_hmac('sha256',bytes(password,"UTF-8"),bytes(salt,"UTF-8"),100000) #type:ignore
+    password=str(binascii.hexlify(password),"UTF-8") #type:ignore
+    return password
+
+if __name__=="__main__":
+    username=input("Username: ")
+    password=getpass.getpass("Password: ")
+    print(hashPassword(username,password))
